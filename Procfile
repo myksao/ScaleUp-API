0e0b82570a7b472301d8b9efa5ba67572fc9cdeb
Salami Joba Manuel <salamialexzandermanuel@gmail.com>
2020-04-10 21:13:26 -0500

    added main in app file

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --pythonpath src app:app --bind 127.0.0.1:8080 --worker-class sanic.worker.GunicornWorker
+web: gunicorn --pythonpath src app:app --bind 0.0.0.0:1337 --worker-class sanic.worker.GunicornWorker
--- src/app.py
+++ src/app.py
@@ -29,8 +29,8 @@ async def subscriptions(request, ws):
     await subscription_server.handle(ws)
     return ws
 
-
-app.run(host="127.0.0.1", port=8080)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=1337,)
 
 
 
