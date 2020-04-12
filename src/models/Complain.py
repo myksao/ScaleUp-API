af3a14bfabdc5b4be924f110ecf053c0657b246c
Salami Joba Manuel <salamialexzandermanuel@gmail.com>
2020-04-12 17:29:16 -0500

    meta added

--- src/models/Complain.py
+++ src/models/Complain.py
@@ -4,7 +4,7 @@ from mongoengine.fields import StringField,FileField,ListField
 
 # main schema
 class Complain(Document):
-    meta = {'collection': 'complain'}
+    meta = {'collection': 'complain','strict':False}
     post: StringField(required = True)
     images: ListField(FileField())
 
