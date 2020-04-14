d5a7f5305e006fa2edb15e40096132283077b2d2
Salami Joba Manuel <salamialexzandermanuel@gmail.com>
2020-04-14 12:32:55 -0500

    no

--- src/app.py
+++ src/app.py
@@ -18,23 +18,24 @@ connect(host=database)
 
 # start sanic async web server
 app = Sanic(__name__)
-subscription_server = WsLibSubscriptionServer(schema)
 
 # Middleware can be added just like js middlewares to modify the request to or response 
 # Listeners helps to execute startup/teardown code as your server starts or closes  
-@app.listener('before_server_start')
+subscription_server = WsLibSubscriptionServer(schema)
+
 @app.websocket('/subscriptions', subprotocols=['graphql-ws'])
-async def init_graphql(app,loop,ws):
+@app.listener('before_server_start')
+def init_graphql(app,loop):
     app.add_route(GraphQLView.as_view(schema=schema,executor= AsyncioExecutor(loop=loop), graphiql=True),'')
+
+
+
+subscription_server = WsLibSubscriptionServer(schema)
+
+@app.websocket('/subscriptions', subprotocols=['graphql-ws'])
+async def subscriptions(request, ws):
     await subscription_server.handle(ws)
     return ws
-
-
-
-# @app.websocket('/subscriptions', subprotocols=['graphql-ws'])
-# async def subscriptions(request, ws):
-#     await subscription_server.handle(ws)
-#     return ws
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=1337,)
