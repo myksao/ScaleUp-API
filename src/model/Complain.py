dd1f261cfbebdd827f3851f323c083ecc7e2589f
Salami Joba Manuel <salamialexzandermanuel@gmail.com>
2020-04-18 11:23:37 -0500

    added _id to complain

--- src/model/Complain.py
+++ src/model/Complain.py
@@ -1,10 +1,11 @@
 from mongoengine import Document
-from mongoengine.fields import StringField,ListField
+from mongoengine.fields import StringField,ObjectIdField,ListField
 
 
 # main schema
 class Complain(Document):
     meta = {'collection': 'complain','strict':False}
+    _id = ObjectIdField()
     post= StringField(required = True)
     images = ListField(StringField())
 
